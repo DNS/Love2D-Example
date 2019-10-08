e6da295c8fa839eb0114862ae2c0daac3712c4b1
SiraitX <SiraitX@localhost>
2019-10-08 18:31:34 +0700

    -

--- LUA Cheat Sheet.lua
+++ LUA Cheat Sheet.lua
@@ -137,6 +137,19 @@ print(var_array[1])
 print(var_array[2])
 print(var_array[3])
 
+-- Copy Table / Array
+function copy(obj, seen)
+  if type(obj) ~= 'table' then return obj end
+  if seen and seen[obj] then return seen[obj] end
+  local s = seen or {}
+  local res = setmetatable({}, getmetatable(obj))
+  s[obj] = res
+  for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
+  return res
+end
+
+
+
 -- class, OOP
 local obj = {}
 
